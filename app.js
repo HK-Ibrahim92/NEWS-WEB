12fac81748de2685c4a2c6a0a6e4d7b0685d6888
Syed Ibrahim <Syedzada786alam@gmail.com>
2023-09-03 10:50:10 +0500

    load more

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-
-
-
-
 let newscall = ((search="general") => {
 
     apikey = ['3b6b1edd12790ccd9235eb73d22359ac','0a53d58fb2c827bdaffe1e237ac4ed04'];
-    url = `https://gnews.io/api/v4/top-headlines?category=${search}&max=10&apikey=${apikey[1]}`;
-  console.log(search)
+    url = `https://gnews.io/api/v4/top-headlines?category=${search}&max=15&apikey=${apikey[1]}`;
+
     fetch(url)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
      var articles=data.articles
-     console.log(data)
+    //  console.log(data)
        
 for(var i=0; i<articles.length; i++){
     const {image,title,description,publishedAt}=articles[i]    
-    console.log(articles[i]);
+    // console.log(articles[i]);
 
 let news=document.getElementById("news")
 
@@ -39,8 +35,20 @@ console.log("error:"+err)
   });
  
  
-  newscall()
-
+ newscall()
+let loadmore=(()=>{
+newscall()
+let news=document.getElementById("news")
+news.innerHTML +=`  <div class="card mb-2" style="width: 18rem;">
+<img src="${image}" class="news-image card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">${title.slice(0,20)}...</h5>
+  <p class="card-text">${description.slice(0,50)}...</p>
+  <span class="badge text-bg-info">${moment(publishedAt).fromNow()}</span>
+</div>
+</div>
+`
+})
 
 var  btnsearch=(()=>{
   news.innerHTML="" 
